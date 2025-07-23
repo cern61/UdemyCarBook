4dcabbcb1bf871da9c43451e76b5c93243ec84c2
Ceren <Ceren@LAPTOP-M3VUGJJR>
2025-07-23 16:07:52 +0300

    Pricings are added

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Commands/PricingCommands/CreatePricingCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Commands.PricingCommands
+{
+    public class CreatePricingCommand : IRequest
+    {
+
+        public string Name { get; set; }
+
+    }
+}
