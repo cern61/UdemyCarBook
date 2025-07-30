4ad4ba6150407f7ae4d464b879d2495574f8c4cc
Ceren <Ceren@LAPTOP-M3VUGJJR>
2025-07-30 15:13:15 +0300

    updated2

--- Frontends/UdemyCarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/UdemyCarBook.WebUI/Controllers/ServiceController.cs
@@ -1,29 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using UdemyCarBook.Dto.ServiceDtos;
-using UdemyCarBook.Dto.TestimonialDtos;
 
 namespace UdemyCarBook.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public ServiceController(IHttpClientFactory httpClientFactory)
+        public IActionResult Index()
         {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7294/api/Services");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
-                return View(values);
-            }
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
             return View();
         }
     }
